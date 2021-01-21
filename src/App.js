533d1b6ce555376bd9d10a4588be1de167a4c1b3
ioilmio <illuminato.salvatore@gmail.com>
2021-01-21 17:30:18 +0100

    delete logo; install prop-types and big.js as dependecies;

--- src/App.js
+++ src/App.js
@@ -1,17 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit
-          {' '}
-          <code>src/App.js</code>
-          {' '}
-          and save to reload.
+          Are there any unecessary files?
+          I just deleted logo.
         </p>
         <a
           className="App-link"
