20af2e75285cd60b09e9a6176e5b4fd5dc67d931
ioilmio <illuminato.salvatore@gmail.com>
2021-01-21 23:17:37 +0100

    install node-sass;setup npm scripts for sass;

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Display from './Display';
 import ButtonPanel from './ButtonPanel';
-import './App.css';
+import '../styles/css/app.css';
 
 const App = () => (
   <>
